c294135e02c91e9274a69890214cd67345c5c90b
Elton Silva <elton.chrls@gmail.com>
2017-03-16 16:12:27 -0300

    Pre merge in city and state

---
+++ app/models/core_address/city.rb
@@ -0,0 +1,5 @@
+module CoreAddress
+  class City < ApplicationRecord
+    self.table_name = 'extranet.address_cities'
+  end
+end
---
+++ app/models/core_address/state.rb
@@ -0,0 +1,5 @@
+module CoreAddress
+  class State < ApplicationRecord
+    self.table_name = 'extranet.address_states'
+  end
+end
